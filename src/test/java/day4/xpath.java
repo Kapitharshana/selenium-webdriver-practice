09460ffc77c4116b28e95c08f0a983e2479da02e
Kapisan <Kapisan@DESKTOP-2TJGH75>
2026-03-11 02:45:57 +0530

    Added new selenium test case practices

--- src/test/java/day4/xpath.java
+++ src/test/java/day4/xpath.java
@@ -13,23 +13,31 @@ public class xpath {
 		driver.get("https://demo.nopcommerce.com/");
 		
 		//x path with single attribute
-		//driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Tshirt");
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Tshirt");
 		
 		//x path with multiple attribute
-		//driver.findElement(By.xpath("//input[@name='q'][@placeholder='Search store']")).sendKeys("Tshirt");
+		driver.findElement(By.xpath("//input[@name='q'][@placeholder='Search store']")).sendKeys("Tshirt");
 
 		//static elements 
 		//and or 
-		//driver.findElement(By.xpath("//input[@name='q' and @placeholder='Search store']")).sendKeys("Tshirt");
-		//driver.findElement(By.xpath("//input[@name='q' or @placeholder='Sea']")).sendKeys("Tshirt");
+		driver.findElement(By.xpath("//input[@name='q' and @placeholder='Search store']")).sendKeys("Tshirt");
+		driver.findElement(By.xpath("//input[@name='q' or @placeholder='Sea']")).sendKeys("Tshirt");
 
 		//x path with linked text and inner text
 		
-		//driver.findElement(By.xpath("//*[text()='Electronics']")).click();
+		driver.findElement(By.xpath("//*[text()='Electronics']")).click();
 		
-		//boolean answer= driver.findElement(By.xpath("//h2[text()='Welcome to our store']")).isDisplayed();
+		boolean answer= driver.findElement(By.xpath("//h2[text()='Welcome to our store']")).isDisplayed();
+		System.out.println(answer);
 		
-		//System.out.println(answer);
-	}
+		//xpath with contains
+		driver.findElement(By.xpath("//input[contains(@placeholder,'Sear')]")).sendKeys("Tshirt");
+		
+		//xpath with contains
+		driver.findElement(By.xpath("//input[starts-with(@placeholder,'Sea')]")).sendKeys("Tshirt");
 
+		//chained xpath
+		boolean state= driver.findElement(By.xpath("//div[@class='picture']/a/img")).isDisplayed();
+		System.out.println(state);
+	}
 }
