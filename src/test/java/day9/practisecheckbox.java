6f5d2a2756db18369a08d49acbe26cb13f293077
Kapitharshana <kapidarsha431@gmail.com>
2026-03-24 13:57:17 +0530

    Merge branch 'main' of https://github.com/Kapitharshana/selenium-webdriver-practice.git into main

---
+++ src/test/java/day9/practisecheckbox.java
@@ -0,0 +1,53 @@
+package day9;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class practisecheckbox {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+		//select specific checkbox
+		//driver.findElement(By.xpath("(//input[@type='checkbox'])[8]")).click();
+		
+		
+		/*List <WebElement> checkboxes= driver.findElements(By.xpath("//tbody//td//input[@type='checkbox']"));
+		
+		for(WebElement  box: checkboxes) {
+			box.click();
+		}*?
+		
+		
+		//select first 2 checkboxes
+		/*List <WebElement> checkboxes= driver.findElements(By.xpath("//tbody//td//input[@type='checkbox']"));
+		for(int i=0; i<2; i++) {
+			checkboxes.get(i).click();
+		}*/
+		
+		//select last 3 
+		List <WebElement> checkboxes= driver.findElements(By.xpath("//tbody//td//input[@type='checkbox']"));
+		for(int i=3; i<checkboxes.size(); i++) {
+			checkboxes.get(i).click();
+		}
+		
+        for(int i=0; i<checkboxes.size(); i++) {
+			
+			if(checkboxes.get(i).isSelected()){
+				
+			checkboxes.get(i).click();
+		}
+			}
+		
+	
+		
+
+	}
+
+}
